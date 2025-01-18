2495e24d3472b5f937663e97911b2a6c90e6066e
seojw0124 <wjddn2000124@gmail.com>
2025-01-19 06:01:17 +0900

    feat: api 연결 성공

--- app/src/main/java/com/example/seton/di/ApiService.kt
+++ app/src/main/java/com/example/seton/di/ApiService.kt
@@ -6,6 +6,8 @@ import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -13,22 +15,16 @@ import java.io.File
 
 interface ApiService {
     @Multipart
-    @POST("/houses") // 서버 엔드포인트 경로
+    @POST("/houses")
     suspend fun uploadData(
-        @Part("title") title: RequestBody,
-        @Part("phoneNumber") phoneNumber: RequestBody,
-        @Part("address") address: RequestBody,
-        @Part("size") size: RequestBody,
-        @Part("description") description: RequestBody,
-        @Part("price") price: RequestBody,
-        @Part("tags") tags: RequestBody,
+        @Part("request") request: RequestBody,
         @Part thumbnail: MultipartBody.Part,
         @Part images: List<MultipartBody.Part>
-    )
+    ): Response<Void>
 }
 
 fun createPartFromString(value: String): RequestBody {
-    return RequestBody.create("text/plain".toMediaTypeOrNull(), value)
+    return value.toRequestBody("text/plain".toMediaTypeOrNull())
 }
 
 //fun prepareFilePart(context: Context, partName: String, fileUri: Uri): MultipartBody.Part {
