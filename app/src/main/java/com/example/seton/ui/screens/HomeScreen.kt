6196e30f6fae92bffbe14eaaed66e369917603c5
hoinhoin <follow3448@naver.com>
2025-01-19 02:26:35 +0900

    bottom filter update

--- app/src/main/java/com/example/seton/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/seton/ui/screens/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.seton.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.content.MediaType.Companion.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,8 +12,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Done
@@ -21,17 +27,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.seton.R
 import com.example.seton.widgets.BottomNavigation
@@ -127,5 +142,111 @@ fun HomeScreen(
 
 @Composable
 fun FilterDialog() {
+
+    val regions = listOf(
+        "강원" to listOf("강릉시","고성군","동해시", "삼척시", "양구군", "양양군", "영월군", "원주시","인제군", "정선군", "철원군","춘천시"),
+        "대전" to listOf("양구군", "양양군")
+    )
+    var selectedMainRegion by remember { mutableStateOf<String?>(null) }
+    var selectedSubRegion by remember { mutableStateOf<String?>(null) }
+
+    Column (
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+
+        ){
+        var sliderPosition by remember { mutableFloatStateOf(0f) }
+        var landarea by rememberSaveable { mutableIntStateOf(10) }
+        Text(
+            text = "지역을 선택해주세요",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(Modifier.fillMaxSize()) {
+            // 메인 리스트
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.LightGray)
+            ) {
+                items(regions.size) { index ->
+                    val regionName = regions[index].first
+
+                    Text(
+                        text = regionName,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .clickable { selectedMainRegion = regionName }
+                            .background(if (selectedMainRegion == regionName) Color.Gray else Color.Transparent),
+                        fontSize = 18.sp
+                    )
+                }
+            }
+
+            // 서브 리스트
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                selectedMainRegion?.let { region ->
+                    val sublist = regions.firstOrNull { it.first == region }?.second ?: emptyList()
+                    items(sublist.size) { index ->
+                        Text(
+                            text = sublist[index],
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp)
+                                .clickable { selectedSubRegion = sublist[index] }
+                                .background(if (selectedSubRegion == sublist[index]) Color.Gray else Color.Transparent),
+                            fontSize = 16.sp
+                        )
+                    }
+                }
+            }
+            Column (
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(start = 16.dp)
+            ){
+                Text(text = "평수")
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    TextButton(onClick = { landarea -= 1 }) {
+                        Text(text = "-")
+                    }
+                    Text(
+                        text = "$landarea"
+                    )
+                    TextButton(onClick = {landarea += 1}) {
+                        Text(text = "+")
+
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = "가격")
+
+                Slider(
+                    value = sliderPosition,
+                    onValueChange = {sliderPosition = it},
+                    steps = 1000,
+                    valueRange = 0f .. 999f
+                )
+                Text(
+                    text = "${sliderPosition.toInt()} 만원",
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+            }
+
+
+        }
+    }
 
 }
