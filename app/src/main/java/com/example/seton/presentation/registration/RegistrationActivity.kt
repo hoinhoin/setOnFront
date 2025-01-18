61f5c3b8c35b4a9f42d6965d60acadaa44b5b5e3
seojw0124 <wjddn2000124@gmail.com>
2025-01-19 02:28:31 +0900

    chore: 상세 화면 디자인 구현 중2

---
+++ app/src/main/java/com/example/seton/presentation/registration/RegistrationActivity.kt
@@ -0,0 +1,27 @@
+package com.example.seton.presentation.registration
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.seton.R
+import com.example.seton.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+
+    private val binding: ActivityRegistrationBinding by lazy {
+        ActivityRegistrationBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registration)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+    }
+}
